77b5a5ec58e4dd1f39b4c459dd391cd1b519b352
Kerim Gokarslan <kgokarsl@cisco.com>
2017-08-24 15:20:19 -0700

    buildvm trigger test only
    Change-Id: Iffad4cccacccbb3df240d1b004c27ae209cec701
    Signed-off-by: Kerim Gokarslan <kgokarsl@cisco.com>

--- nfvbenchvm/dib/build-image.sh
+++ nfvbenchvm/dib/build-image.sh
@@ -5,18 +5,26 @@
 # The following packages must be installed prior to using this script:
 # sudo apt-get -y install python-virtualenv qemu-utils kpartx
 
+# Artifact URL
+gs_url=artifacts.opnfv.org/nfvbench
 # image version number
 __version__=0.3
-image_name=nfvbenchvm_centos-$__version__
+#image_name=nfvbenchvm_centos-$__version__
+image_name=tst.txt
+# if image exists already skip building
+if  gsutil -q stat gs://$gs_url/$image_name; then
+	echo "Image already exists at http://$gs_url/$image_name.qcow2"
+	exit 0
+fi
 
 # install diskimage-builder
-if [ -d dib-venv ]; then
-    . dib-venv/bin/activate
-else
-    virtualenv dib-venv
-    . dib-venv/bin/activate
-    pip install diskimage-builder
-fi
+#if [ -d dib-venv ]; then
+#    . dib-venv/bin/activate
+#else
+#    virtualenv dib-venv
+#    . dib-venv/bin/activate
+#    pip install diskimage-builder
+#fi
 
 # Add nfvbenchvm_centos elements directory to the DIB elements path
 export ELEMENTS_PATH=`pwd`/elements
@@ -36,6 +44,6 @@ export DIB_YUM_REPO_CONF=$ELEMENTS_PATH/nfvbenchvm/fdio-release.repo
 export DIB_USE_ELREPO_KERNEL=True
 
 echo "Building $image_name.qcow2..."
-time disk-image-create -o $image_name centos7 nfvbenchvm
-
-ls -l $image_name.qcow2
+#time disk-image-create -o $image_name centos7 nfvbenchvm
+gsutil cp tst.txt gs://$gs_url/$image_name
+#ls -l $image_name.qcow2

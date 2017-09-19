ac8dcddc9e11c958a1e42839793a5fa1e207143c
Kerim Gokarslan <kgokarsl@cisco.com>
2017-09-19 15:33:46 -0700

    NFVBENCH-28 Set result values for NDR/PDR if they cannot be found within the given epsilon
    Change-Id: Id8019c7f052c2b189f3ab6e39377f65fe00d9fee
    Signed-off-by: Kerim Gokarslan <kgokarsl@cisco.com>

--- docs/testing/user/userguide/conf.py
+++ docs/testing/user/userguide/conf.py
@@ -1,5 +1,17 @@
 # Copyright 2017 Cisco Systems, Inc.  All rights reserved.
 #
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
 # NFVBench documentation build configuration file, created by
 # sphinx-quickstart on Thu Sep 29 14:25:18 2016.
 #
@@ -16,6 +28,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+
+
+
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))

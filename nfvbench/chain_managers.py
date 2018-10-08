99260f95219301bb5c0b58921e793bcad6ec4990
ahothan <ahothan@cisco.com>
2018-10-08 09:34:00 -0700

    NFVBENCH-102 NFVBench won't work with external chain
    Change-Id: I47e9d128bca883573ea1b1e21ba49dc0305edcbb
    Signed-off-by: ahothan <ahothan@cisco.com>

--- nfvbench/chain_managers.py
+++ nfvbench/chain_managers.py
@@ -81,7 +81,10 @@ class PVPStatsManager(object):
         self._setup()
 
     def set_vlan_tag(self, device, vlan):
-        self.worker.set_vlan_tag(device, vlan)
+        if self.worker:
+            self.worker.set_vlan_tag(device, vlan)
+        else:
+            device.set_vlan_tag(vlan)
 
     def _setup(self):
         WORKER_CLASS = self.factory.get_chain_worker(self.specs.openstack.encaps,

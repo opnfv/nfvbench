09363d4f844128999780f6e287fc57662ca3b42a
Yichen Wang <yicwang@cisco.com>
2019-08-04 20:24:32 -0700

    Release the validation check for VxLAN networks
    Change-Id: I9583d4d10e704ead63ea95d2d2842c3d00a85a3b
    Signed-off-by: Yichen Wang <yicwang@cisco.com>

--- nfvbench/chaining.py
+++ nfvbench/chaining.py
@@ -352,8 +352,7 @@ class ChainNetwork(object):
 
         :return: VNI ID for this network
         """
-        if 'vxlan' not in self.network['provider:network_type']:
-            raise ChainException('Trying to retrieve VNI for non VXLAN network')
+
         return self.network['provider:segmentation_id']
 
     def delete(self):

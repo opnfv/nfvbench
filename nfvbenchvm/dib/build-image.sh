ad9c5ed6a8e682547b824c7c5c1bf2caeb73c5bf
fmenguy <francoisregis.menguy@orange.com>
2021-05-17 14:01:20 +0200

    Fix six ensure_text error
    Change-Id: I0715b46b34b382176f2e06798deeede402b4c2fd
    Signed-off-by: fmenguy <francoisregis.menguy@orange.com>

--- nfvbenchvm/dib/build-image.sh
+++ nfvbenchvm/dib/build-image.sh
@@ -116,18 +116,13 @@ function build_image {
 
     ls -l $1.qcow2
 
-    if [ $verify_only -eq 1 ]; then
-        echo "Image verification SUCCESS"
-        echo "NO upload to google storage (-v)"
+    if command -v gsutil >/dev/null; then
+        echo "Uploading $1.qcow2..."
+        gsutil cp $1.qcow2 gs://$gs_url/$1.qcow2
+        echo "You can access to image at http://$gs_url/$1.qcow2"
     else
-        if command -v gsutil >/dev/null; then
-            echo "Uploading $1.qcow2..."
-            gsutil cp $1.qcow2 gs://$gs_url/$1.qcow2
-            echo "You can access to image at http://$gs_url/$1.qcow2"
-        else
-            echo "Cannot upload new image to the OPNFV artifact repository (gsutil not available)"
-            exit 1
-        fi
+        echo "Cannot upload new image to the OPNFV artifact repository (gsutil not available)"
+        exit 1
     fi
 }
 
